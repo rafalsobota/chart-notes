f6d26f9ae9e9a1b63fb6934f8f2095e9b82eceaf
Rafał Sobota <ravsobota@gmail.com>
2022-03-31 01:31:53 +0200

    Change empty chart icon

--- components/EmptyChart.tsx
+++ components/EmptyChart.tsx
@@ -1,4 +1,4 @@
-import ChartBarIcon from "@heroicons/react/outline/ChartBarIcon";
+import { DatabaseIcon } from "@heroicons/react/outline";
 
 type EmptyChartProps = {
   message: string;
@@ -7,7 +7,7 @@ type EmptyChartProps = {
 export const EmptyChart: React.FC<EmptyChartProps> = ({ message }) => {
   return (
     <div className="flex flex-row items-center justify-center w-full h-[300px] p-10 rounded-md bg-white text-gray-500">
-      <ChartBarIcon className="h-5 mr-1" />
+      <DatabaseIcon className="h-5 mr-1" />
       <div>{message}</div>
     </div>
   );
